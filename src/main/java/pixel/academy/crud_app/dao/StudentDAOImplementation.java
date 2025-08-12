2b0abc6a69672add4ccfcfc5c10ecd6fd8af1d96
Sorin Gore <soringoredev@gmail.com>
2025-08-12 19:05:52 +0300

    fix: Solve return deleted rows problem

--- src/main/java/pixel/academy/crud_app/dao/StudentDAOImplementation.java
+++ src/main/java/pixel/academy/crud_app/dao/StudentDAOImplementation.java
@@ -63,7 +63,7 @@ public class StudentDAOImplementation implements StudentDAO {
     public int deleteAll() {
 
         int numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
-        return 0;
+        return numRowsDeleted;
 
     }
 
